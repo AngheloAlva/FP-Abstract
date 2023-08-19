3366bde0294920a6a42117cfeab39748c8422250
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-19 13:06:03 -0400

    add footer style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,52 +1,52 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { Logo } from './assets/Logo'
+import './footer.css'
 
 export const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className='footer-section'>
         <ul>
-          <li><h4>Abstract</h4></li>
-          <li><a>Start Trial</a></li>
-          <li><a>Pricing</a></li>
-          <li><a>Download</a></li>
+          <li><h3>Abstract</h3></li>
+          <li><a href='#'>Start Trial</a></li>
+          <li><a href='#'>Pricing</a></li>
+          <li><a href='#'>Download</a></li>
         </ul>
         <ul>
-          <li><h4>Resources</h4></li>
-          <li><a>Blog</a></li>
-          <li><a>Help Center</a></li>
-          <li><a>Release Notes</a></li>
-          <li><a>Status</a></li>
+          <li><h3>Resources</h3></li>
+          <li><a href='#'>Blog</a></li>
+          <li><a href='#'>Help Center</a></li>
+          <li><a href='#'>Release Notes</a></li>
+          <li><a href='#'>Status</a></li>
+        </ul>
+        <ul>
+          <li><h3>Community</h3></li>
+          <li><a href='#'>Twitter</a></li>
+          <li><a href='#'>LinkedIn</a></li>
+          <li><a href='#'>Facebook</a></li>
+          <li><a href='#'>Dribble</a></li>
+          <li><a href='#'>Podcast</a></li>
+        </ul>
+        <ul>
+          <li><h3>Company</h3></li>
+          <li><a href='#'>About Us</a></li>
+          <li><a href='#'>Careers</a></li>
+          <li><a href='#'>Legal</a></li>
+          <li><a href='#'>Status</a></li>
+
+          <li style={{ marginTop: '1.8rem' }}><strong>Contact Us</strong></li>
+          <li><a href='#'>info@abstract.com</a></li>
         </ul>
       </div>
 
-      <div>
-        <ul>
-          <li><h4>Community</h4></li>
-          <li><a>Twitter</a></li>
-          <li><a>LinkedIn</a></li>
-          <li><a>Facebook</a></li>
-          <li><a>Dribble</a></li>
-          <li><a>Podcast</a></li>
-        </ul>
-        <ul>
-          <li><h4>Company</h4></li>
-          <li><a>About Us</a></li>
-          <li><a>Careers</a></li>
-          <li><a>Legal</a></li>
-          <li><a>Status</a></li>
-
-          <li><h5>Contact Us</h5></li>
-          <li><a>info@abstract.com</a></li>
-        </ul>
-      </div>
-
-      <div>
+      <div className='static-info'>
         <Logo />
-        <h4>© Copyright 2023</h4>
-        <p>Abstract Studio Desingn, Inc.</p>
-        <p>All rights reserved</p>
+        <div>
+          <h4>© Copyright 2023</h4>
+          <p>Abstract Studio Desingn, Inc.</p>
+          <p>All rights reserved</p>
+        </div>
       </div>
     </footer>
   )
